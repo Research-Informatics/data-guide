1c72c45ead5042a68a87938cd931130ae4271052
mhofmanbmc <85198346+mhofmanbmc@users.noreply.github.com>
2024-11-19 07:03:53 -0500

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,9 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
         link.addEventListener('click', (e) => {
             e.preventDefault(); // Prevent default link behavior
             const target = document.querySelector(link.getAttribute('data-target'));
+            const arrow = link.querySelector('.toggle-arrow'); // Select the arrow span
 
             if (target) {
                 target.classList.toggle('active'); // Toggle the "active" class
+                if (arrow) {
+                    arrow.classList.toggle('expanded'); // Toggle arrow rotation
+                }
             }
         });
     });
